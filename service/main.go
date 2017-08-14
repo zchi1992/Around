4c42cfaf08fd4958d3ac0332e20ce18facab00cc
zchi1992 <zchi1992@hotmail.com>
2017-08-13 23:12:36 -0400

    change port to 8080

--- service/main.go
+++ service/main.go
@@ -61,7 +61,7 @@ func main() {
 	fmt.Println("started-service")
 	http.HandleFunc("/post", handlerPost)
 	http.HandleFunc("/search", handlerSearch)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
