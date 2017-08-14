b4b0278abcf19c7e6d2bada16f00b8db36266e61
zchi1992 <zchi1992@hotmail.com>
2017-08-13 23:32:50 -0400

    update port to 8080

--- service/main.go
+++ service/main.go
@@ -9,8 +9,8 @@ import (
 	"strconv"
 	"reflect"
 	"github.com/pborman/uuid"
-	// "context"
-	// "cloud.google.com/go/bigtable"
+	 "context"
+	 "cloud.google.com/go/bigtable"
 )
 
 type Location struct {
@@ -99,31 +99,31 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("Post is saved to Index: %s\n", p.Message)
 
-	//ctx := context.Background()
-	//// you must update project name here
-	//bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
-	//if err != nil {
-	//	panic(err)
-	//	return
-	//}
+	ctx := context.Background()
+	// you must update project name here
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
 
 
-	//// TODO (student questions) save Post into BT as well
-	//tbl := bt_client.Open("post")
-	//mut := bigtable.NewMutation()
-	//t := bigtable.Now()
-	//
-	//mut.Set("post", "user", t, []byte(p.User))
-	//mut.Set("post", "message", t, []byte(p.Message))
-	//mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
-	//mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
-	//
-	//err = tbl.Apply(ctx, id, mut)
-	//if err != nil {
-	//	panic(err)
-	//	return
-	//}
-	//fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
+	// TODO (student questions) save Post into BT as well
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 
 }
 
